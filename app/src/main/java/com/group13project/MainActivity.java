626b8418c0cf1caf2d39fbc879d49bcbe621f3af
Seif ELBayomi <sf693072@dal.ca>
2023-02-02 15:51:12 -0400

    Revert "Merge branch 'Mohammed_Jasim' into 'main'"
    This reverts merge request !2

--- app/src/main/java/com/group13project/MainActivity.java
+++ app/src/main/java/com/group13project/MainActivity.java
@@ -1,36 +1,13 @@
 package com.group13project;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button signUpButton;
-    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        signUpButton = (Button)findViewById(R.id.signUpButton);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent action = new Intent(MainActivity.this, signup_page.class);
-                startActivity(action);
-            }
-        });
-
-        loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }

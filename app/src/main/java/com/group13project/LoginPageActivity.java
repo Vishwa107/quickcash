2cb34bed3292c4868004595ecf123fe8ad4948be
Mohammed Alajmi <mohammed.ajmi@dal.ca>
2023-02-14 20:09:08 -0400

    Added getter methods to the fields in SignupPageActivity and LoginPageActivity.java. Also minor changes to ids in loginpage.xml.

--- app/src/main/java/com/group13project/LoginPageActivity.java
+++ app/src/main/java/com/group13project/LoginPageActivity.java
@@ -4,6 +4,7 @@ package com.group13project;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 
 public class LoginPageActivity extends Activity {
 
@@ -11,5 +12,15 @@ public class LoginPageActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+    }
+
+    protected String getEmailAddress() {
+        EditText emailAddress = findViewById(R.id.emailField);
+        return emailAddress.getText().toString().trim();
+    }
+
+    protected String getPassword() {
+        EditText password = findViewById(R.id.passwordField);
+        return password.getText().toString().trim();
     }
 }
--- app/src/main/java/com/group13project/SignupPageActivity.java
+++ app/src/main/java/com/group13project/SignupPageActivity.java
@@ -3,6 +3,7 @@ package com.group13project;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 
 public class SignupPageActivity extends Activity {
     @Override
@@ -10,4 +11,36 @@ public class SignupPageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
     }
+
+    protected String getEmailAddress() {
+        EditText emailAddress = findViewById(R.id.emailAddress);
+        return emailAddress.getText().toString().trim();
+    }
+
+    protected String getPhoneNumber() {
+        EditText phoneNumber = findViewById(R.id.phoneNumber);
+        return phoneNumber.getText().toString().trim();
+    }
+
+    protected String getFirstName() {
+        EditText firstName = findViewById(R.id.firstName);
+        return firstName.getText().toString().trim();
+    }
+
+    protected String getLastName() {
+        EditText lastName = findViewById(R.id.lastName);
+        return lastName.getText().toString().trim();
+    }
+
+    protected String getPassword() {
+        EditText password = findViewById(R.id.password1);
+        return password.getText().toString().trim();
+    }
+
+    protected String getPasswordConfirmation() {
+        EditText password2 = findViewById(R.id.password2);
+        return password2.getText().toString().trim();
+    }
+
+
 }

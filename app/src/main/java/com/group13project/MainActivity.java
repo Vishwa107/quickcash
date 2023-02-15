cd82ef887ab1ada0e6eaa6934d15b7a90c198db4
elbayomi <sf693072@dal.ca>
2023-02-14 21:12:28 -0400

    Solve FireBase Connection Error

--- app/src/main/java/com/group13project/MainActivity.java
+++ app/src/main/java/com/group13project/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     Button signUpButton;
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue("Hello, World!");
 
         signUpButton = (Button)findViewById(R.id.signUpButton);
         signUpButton.setOnClickListener(new View.OnClickListener() {

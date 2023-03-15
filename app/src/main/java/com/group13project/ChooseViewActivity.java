0329c4d7305d8265c9e056832b8a1daa4943227a
Khaleda Efat Jahan <Kh715412@dal.ca>
2023-03-15 13:22:07 +0600

    added choose view functionality

---
+++ app/src/main/java/com/group13project/ChooseViewActivity.java
@@ -0,0 +1,37 @@
+package com.group13project;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseViewActivity extends AppCompatActivity{
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_view_activity);
+        setTitle("Choose View");
+
+        Button employer_view=(Button) findViewById(R.id.employer_view);
+        employer_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ChooseViewActivity.this,EmployerHomeActivity.class));
+            }
+        });
+
+        Button employee_view=(Button) findViewById(R.id.employee_view);
+        employee_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ChooseViewActivity.this,EmployeeHomeActivity.class));
+            }
+        });
+    }
+
+}

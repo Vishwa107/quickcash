a3b732d469b896c52e9d952e654284d455652a09
Vishwa Patel <vs450741@dal.ca>
2023-03-16 17:23:20 -0300

    added functionality to the buttons on employerHomeActivity and retrieved user's data for the profile page

--- app/src/main/java/com/group13project/EmployerProfile.java
+++ app/src/main/java/com/group13project/EmployerProfile.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,31 +20,38 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class EmployerProfile extends Activity {
-    Button newJob;
-    Button postedJob;
-    static String empDetails = new String();
-    FirebaseDatabase database = null;
-    DatabaseReference firstNameRef;
-    DatabaseReference lastNameRef;
-    DatabaseReference emailRef;
-    DatabaseReference phoneNumberRef;
+    String empDetails = "";
+    FirebaseUser user = null;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_profile);
 
-        // Initialize the newJob button
-        newJob = findViewById(R.id.newJob);
+        TextView details = (TextView)findViewById(R.id.empDetails);
 
-        // Set up an OnClickListener for the newJob button
-        newJob.setOnClickListener(new View.OnClickListener() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        String userID = user.getUid();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users/"+ userID);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                // Start EmployerNewPostActivity
-                Intent intent = new Intent(EmployerProfile.this, EmployerNewPostActivity.class);
-                startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User current = snapshot.getValue(User.class);
+                String neww = current.firstName + "\n" + current.lastName + "\n" + current.phoneNumber + "\n" + current.emailAddress;
+                details.setText(neww);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                final String errorRead = error.getMessage();
+                details.setText(errorRead);
             }
         });
+
     }
+
 }
